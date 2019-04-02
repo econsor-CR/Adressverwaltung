8a5e2dccec442353dd25f2b839ddbb2b72dd3cc1
econsor-CR <45256947+econsor-CR@users.noreply.github.com>
2019-04-02 13:21:21 +0200

    added Database and empty form

---
+++ AdressverwaltungDB/adressen.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[adressen]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] NVARCHAR(MAX) NOT NULL, 
+    [vorname] NVARCHAR(MAX) NOT NULL, 
+    [email] NVARCHAR(MAX) NOT NULL, 
+    [telefonnummer] INT NOT NULL, 
+    [strasse] NVARCHAR(MAX) NOT NULL, 
+    [hausnummer] NVARCHAR(MAX) NOT NULL, 
+    [postleitzahl] INT NOT NULL, 
+    [ort] NVARCHAR(MAX) NOT NULL
+)
