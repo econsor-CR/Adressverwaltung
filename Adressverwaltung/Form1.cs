8993e92d19d0d150c35b435198d025004027967e
econsor-CR <45256947+econsor-CR@users.noreply.github.com>
2019-04-02 13:30:23 +0200

    added regex to validate mail address

--- Adressverwaltung/Form1.cs
+++ Adressverwaltung/Form1.cs
@@ -16,5 +16,10 @@ namespace Adressverwaltung
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ^(?(")(".+?(?<!\\)"@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$
+        }
     }
 }
